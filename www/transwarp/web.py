039cc5877a1d0561c7ebde77ec95326c285c47f7
twoc <chenchen0432@gmail.com>
2015-03-24 07:28:44 +0800

    modified web.py

--- www/transwarp/web.py
+++ www/transwarp/web.py
@@ -284,7 +284,156 @@ class RedirectError(HttpError):
 
     __repr__ = __str__
 
+def badrequest():
+    '''
+    Send a bad request response.
+
+    >>> raise badrequest()
+    Traceback (most recent call last):
+      ...
+    HttpError: 400 Bad Request
+    '''
+    return HttpError(400)
+
+def unauthorized():
+    '''
+    Send an unauthorized response.
+
+    >>> raise unauthorized()
+    Traceback (most recent call last):
+      ...
+    HttpError: 401 Unauthorized
+    '''
+    return HttpError(401)
+
+def forbidden():
+    '''
+    Send a forbidden response.
+
+    >>> raise forbidden()
+    Traceback (most recent call last):
+      ...
+    HttpError: 403 Forbidden
+    '''
+    return HttpError(403)
+
+def notfound():
+    '''
+    Send a not found response.
+
+    >>> raise notfound()
+    Traceback (most recent call last):
+      ...
+    HttpError: 404 Not Found
+    '''
+    return HttpError(404)
+
+def conflict():
+    '''
+    Send a conflict response.
+
+    >>> raise conflict()
+    Traceback (most recent call last):
+      ...
+    HttpError: 409 Conflict
+    '''
+    return HttpError(409)
+
+def internalerror():
+    '''
+    Send an internal error response.
+
+    >>> raise internalerror()
+    Traceback (most recent call last):
+      ...
+    HttpError: 500 Internal Server Error
+    '''
+    return HttpError(500)
+
+def redirect(location):
+    '''
+    Do permanent redirect.
+
+    >>> raise redirect('http://www.itranswarp.com/')
+    Traceback (most recent call last):
+      ...
+    RedirectError: 301 Moved Permanently, http://www.itranswarp.com/
+    '''
+    return RedirectError(301, location)
+
+def found(location):
+    '''
+    Do temporary redirect.
+
+    >>> raise found('http://www.itranswarp.com/')
+    Traceback (most recent call last):
+      ...
+    RedirectError: 302 Found, http://www.itranswarp.com/
+    '''
+    return RedirectError(302, location)
+
+def seeother(location):
+    '''
+    Do temporary redirect.
+
+    >>> raise seeother('http://www.itranswarp.com/')
+    Traceback (most recent call last):
+      ...
+    RedirectError: 303 See Other, http://www.itranswarp.com/
+    >>> e = seeother('http://www.itranswarp.com/seeother?r=123')
+    >>> e.location
+    'http://www.itranswarp.com/seeother?r=123'
+    '''
+    return RedirectError(303, location)
     
+def get(path):
+    '''
+    A @get decorator.
+
+    @get('/:id')
+    def index(id):
+        pass
+
+    >>> @get('/test/:id')
+    ... def test():
+    ...     return 'ok'
+    ...
+    >>> test.__web_route__
+    '/test/:id'
+    >>> test.__web_method__
+    'GET'
+    >>> test()
+    'ok'
+    '''
+    def _decorator(func):
+        func.__web_route__ = path
+        func.__web_method__ = 'GET'
+        return func
+    return _decorator
+
+def post(path):
+    '''
+    A @post decorator.
+
+    >>> @post('/post/:id')
+    ... def testpost():
+    ...     return '200'
+    ...
+    >>> testpost.__web_route__
+    '/post/:id'
+    >>> testpost.__web_method__
+    'POST'
+    >>> testpost()
+    '200'
+    '''
+    def _decorator(func):
+        func.__web_route__ = path
+        func.__web_method__ = 'POST'
+        return func
+    return _decorator
+
+_re_route = re.compile(r'(\:[a-zA-Z_]\w*)')
+
     
 if __name__=='__main__':
     import doctest
